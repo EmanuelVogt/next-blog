003ee78634a5d84dd5ff61b9f084187aa59729e5
Emanuel Vogt <contatoemanuel3@gmail.com>
2022-09-27 00:49:43 -0300

    feat: envolves components in a layout shell

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,28 @@
-import type { GetServerSideProps, NextPage } from 'next'
-import { withSSRGuest } from '../utils/withSSRGuest'
+import type { GetServerSideProps, NextPage } from "next";
+import { ReactElement } from "react";
+import HomeLayout from "../components/HomeLayout";
+
+import { withSSRGuest } from "../utils/withSSRGuest";
+import { NextPageWithLayout } from "./_app";
 
 const Home: NextPage = () => {
   return (
-    <h1>a</h1>
-  )
-}
+    <h1></h1>
+  );
+};
+// @ts-ignore
+Home.getLayout = (page: ReactElement): ReactElement => (
+  <HomeLayout>
+    {page}
+  </HomeLayout>
+)
 
-export default Home
+export default Home;
 
-export const getServerSideProps: GetServerSideProps = withSSRGuest(async (ctx) => {
-  return {
-    props: {}
+export const getServerSideProps: GetServerSideProps = withSSRGuest(
+  async (ctx) => {
+    return {
+      props: {},
+    };
   }
-})
+);
---
+++ src/pages/me/index.tsx
@@ -0,0 +1,30 @@
+import { Container, Paper } from "@mui/material";
+import { GetServerSideProps, NextPage } from "next";
+import { ReactElement } from "react";
+import HomeLayout from "../../components/HomeLayout";
+import ProfileHome from "../../components/ProfileHome";
+import { withSSRGuest } from "../../utils/withSSRGuest";
+
+const Me: NextPage = () => {
+  return (
+    <ProfileHome />
+  );
+}
+
+// @ts-ignore
+Me.getLayout = (page: ReactElement): ReactElement => (
+  <HomeLayout>
+    {page}
+  </HomeLayout>
+)
+
+export default Me;
+
+
+export const getServerSideProps: GetServerSideProps = withSSRGuest(
+  async (ctx) => {
+    return {
+      props: {},
+    };
+  }
+);
