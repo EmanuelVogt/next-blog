79abdf4bc1c4ed4cbcd35f2f942b2b43a9081374
Emanuel Vogt <contatoemanuel3@gmail.com>
2022-09-27 13:23:30 -0300

    feat: create Post component

---
+++ src/pages/post/[id].tsx
@@ -0,0 +1,28 @@
+import type { GetServerSideProps, NextPage } from "next";
+import { ReactElement } from "react";
+import HomeLayout from "../../components/HomeLayout";
+
+import { withSSRGuest } from "../../utils/withSSRGuest";
+
+const Post: NextPage = () => {
+  return (
+    <h1> aaawad</h1>
+  );
+};
+
+// @ts-ignore
+Post.getLayout = (page: ReactElement): ReactElement => (
+  <HomeLayout>
+    {page}
+  </HomeLayout>
+)
+
+export default Post;
+
+export const getServerSideProps: GetServerSideProps = withSSRGuest(
+  async (ctx) => {
+    return {
+      props: {},
+    };
+  }
+);
