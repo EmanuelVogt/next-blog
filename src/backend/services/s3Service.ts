31a657e899abbf1bcb8881542cb8e90a6449a619
Emanuel Vogt <contatoemanuel3@gmail.com>
2022-09-29 17:00:07 -0300

    feat: create upload to s3 service

---
+++ src/backend/services/s3Service.ts
@@ -0,0 +1,25 @@
+import fs from 'fs'
+
+import AWS from 'aws-sdk'
+
+class s3Service {
+  async uploadFile(fileName, filePath, fileMimetype, s3Path) {
+    const s3 = new AWS.S3({
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      region: process.env.AWS_REGION
+    })
+    const fileContent = fs.readFileSync(filePath)
+
+    const params = {
+      Bucket: process.env.AWS_S3_BUCKET,
+      Key: s3Path ? `${s3Path}/${fileName}` : filePath,
+      Body: fileContent,
+      ContentType: fileMimetype
+    }
+
+    await s3.upload(params).promise()
+    return `${process.env.FILE_PRE_LINK}${s3Path}/${fileName}`
+  }
+}
+export default new s3Service()
