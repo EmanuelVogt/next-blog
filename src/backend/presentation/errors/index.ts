660dc95f1fc042bf49de4471df92f763a4ca6573
Emanuel Vogt <contatoemanuel3@gmail.com>
2022-09-29 18:44:53 -0300

    feat: creat utils erros

---
+++ src/backend/presentation/errors/index.ts
@@ -0,0 +1,6 @@
+export * from './invalid-param-error'
+export * from './missing-param-error'
+export * from './unautorized-error'
+export * from './server-error'
+export * from './forbiden-error'
+export * from './access-denied-error'
