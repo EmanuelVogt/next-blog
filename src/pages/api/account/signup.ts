aec9dc655258efe88ba95deae423cdd63492581f
Emanuel Vogt <contatoemanuel3@gmail.com>
2022-10-01 02:18:03 -0300

    fix: import

--- src/pages/api/account/signup.ts
+++ src/pages/api/account/signup.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { createRouter } from 'next-connect'
-import { makeSignUpControllerFactory } from "@main/factories/controllers/account/signup-controller-factory"
+import { makeSignUpControllerFactory } from "@main/factories/controllers/signup-controller-factory"
 import { adapterRoute } from '@main/next-adapters/route-adapter'
 
 const router = createRouter<NextApiRequest, NextApiResponse>()
